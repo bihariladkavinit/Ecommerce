0d16c8ee7b95926a6126ff517cd2358e148bb20f
bihariladkavinit <bihariladkavinit@gmail.com>
2026-09-06 17:17:04 +0530

    Eureka server changes

---
+++ eureka-server/src/test/java/com/ecomm/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ecomm.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifies that the Spring application context starts successfully
+        // with the Eureka Server auto-configuration applied.
+        // A context startup failure will fail this test automatically.
+    }
+}
